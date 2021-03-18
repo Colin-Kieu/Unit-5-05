007b9e7f552c62e35501e50e86d0631aeb3745d6
ColinKieu <colin.kieu@ocsbstudent.ca>
2021-03-18 14:54:48 +0000

    linter

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ document.getElementById('button').addEventListener('click', multiplication)
 /* this will give the calculate button a function to do, so when the button is clicked it will complete the funtion as we have the event listener */
 function multiplication () {
   number = document.getElementById('textbox').value
- /* this is our function, it takes the value that is put in the text box and will store it */
+  /* this is our function, it takes the value that is put in the text box and will store it */
   number = number * number
- /* this is when it will take the number that is put into the box and will multiply it by itself to get the answer */
+  /* this is when it will take the number that is put into the box and will multiply it by itself to get the answer */
   alert(number)
 }
 /* the answer will show up using an alert box */
